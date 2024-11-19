53594e50f424679c67dfaa02ba040cca196b8470
manuchekhrY <myakh2021@gmail.com>
2024-11-19 23:33:00 +0100

    product class created

---
+++ src/main/java/com/example/demo/model/Product.java
@@ -0,0 +1,23 @@
+package com.example.demo.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+
+    @Id
+    private Long id;
+    private String name;
+    private String description;
+    private BigDecimal price;
+
+}
